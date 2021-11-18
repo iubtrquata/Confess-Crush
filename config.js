e51139a213b00fefdae52d600126e1b9d5e1d648
iubtrquata <94583933+iubtrquata@users.noreply.github.com>
2021-11-18 09:58:47 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,17 @@
 const CONFIG = {
-    titleWeb: "Tui iu b lắm ó btr :33",
-    introTitle: 'Cạm ưn b đã iu tuiii uwu',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: '^^HiHi^^',
+    titleWeb: "Vào đây nà thích hp gòi hyhy",
+    introTitle: 'B iu hay không thì ấn vào ròi bik',
+    introDesc: `Thách ai nói yêu hà phưn đấy?:333`,
+    btnIntro: 'Hellu:333',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
+    btnYes: 'Tui cũm iu b lắm ó hp',
+    btnNo: 'Không còn lâu mới yêu ^^',
     question: 'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
     btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+    reply: 'Vì hà phương cute wa mình hum chịu đượt á hihi^^',
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
     messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
-    btnAccept: 'Okiiiii lun <3',
+    btnAccept: 'Cạm ưn b đã iu tuiiii',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
